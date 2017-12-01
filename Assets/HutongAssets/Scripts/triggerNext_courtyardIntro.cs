ede24f8d18dbb3b2c1cf955c58ed34c4b94b3853
gufy13 <gufy13@yahoo.com>
2017-12-01 10:04:45 -0800

    jingShang scene in place

--- Assets/HutongAssets/Scripts/triggerNext_courtyardIntro.cs
+++ Assets/HutongAssets/Scripts/triggerNext_courtyardIntro.cs
@@ -7,8 +7,17 @@ public class triggerNext_courtyardIntro : MonoBehaviour
 	
 	public void jingshan2courtyardIntro ()
 	{
-		SceneManager.LoadSceneAsync (4);
+
+			StartCoroutine(PlayFadeOutSwitchScene ());
+
 	}
-		
+
+	IEnumerator PlayFadeOutSwitchScene()
+	{
+		hutongOVRScreenFade fadingObj = FindObjectOfType<hutongOVRScreenFade> ();// cameraObj.GetComponent<hutongOVRScreenFade> ();
+		fadingObj.TriggerFadeOut ();
+		yield return new WaitForSeconds (fadingObj.fadeTime); 
+		SceneManager.LoadSceneAsync (4);	
+	}
 
 }
